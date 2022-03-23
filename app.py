283d78ef92341180ebd1f33e52f97adece332771
pyrhaa <sboubtane@protonmail.com>
2022-03-23 15:13:51 +0100

    define the add post method for new todo

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -19,6 +19,16 @@ def index():
     todo_list = Todo.query.all()
     print(todo_list)
     return render_template('base.html', todo_list=todo_list)
+
+
+@app.route('/add', methods=['POST'])
+def add():
+    # add a new todo
+    title = request.form.get('title')
+    new_todo = Todo(title=title, complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
